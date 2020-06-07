201c038ad0aeeff9e91b72918b3f06bb362dc3a1
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-07 15:35:35 +0100

    test: Unit test countScopingBrackets function
    Issue: #5

---
+++ tests/unit/countScopingBrackets.test.js
@@ -0,0 +1,22 @@
+import countScopingBrackets from '../../js/countScopingBrackets';
+
+describe('returns correct number of scoping brackets', () => {
+  test('no brackets', () => {
+    expect(countScopingBrackets([2, '+', 2], 1)).toBe(0);
+  });
+  test('outer brackets only', () => {
+    expect(countScopingBrackets(['(', 2, '+', 2, ')'], 1)).toBe(2);
+  });
+  test('right nested brackets, widest operator', () => {
+    expect(countScopingBrackets(['(', '(', 2, '+', 2, ')', '-', 2, ')'], 2)).toBe(4);
+  });
+  test('right nested brackets, shallowest operator', () => {
+    expect(countScopingBrackets(['(', '(', 2, '+', 2, ')', '-', 2, ')'], 6)).toBe(3);
+  });
+  test('left nested brackets, widest operator', () => {
+    expect(countScopingBrackets(['(', 2, '+', '(', 2, '-', 2, ')', ')'], 2)).toBe(3);
+  });
+  test('left nested brackets, shallowest operator', () => {
+    expect(countScopingBrackets(['(', 2, '+', '(', 2, '-', 2, ')', ')'], 5)).toBe(4);
+  });
+});
