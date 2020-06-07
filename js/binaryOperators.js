64fc7dcef0f66a9812024f7de1e6a967710313d8
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-07 15:29:12 +0100

    feat: Separate binary operations from operators

---
+++ js/binaryOperators.js
@@ -0,0 +1,8 @@
+const binaryOperators = {
+  '+': (leftOperand, rightOperand) => leftOperand + rightOperand,
+  '−': (leftOperand, rightOperand) => leftOperand - rightOperand,
+  '×': (leftOperand, rightOperand) => leftOperand * rightOperand,
+  '÷': (leftOperand, rightOperand) => leftOperand / rightOperand,
+};
+
+export default binaryOperators;
--- js/performBinaryOperation.js
+++ js/performBinaryOperation.js
@@ -1,8 +1,23 @@
-const performBinaryOperation = {
-  '+': (leftOperand, rightOperand) => leftOperand + rightOperand,
-  '−': (leftOperand, rightOperand) => leftOperand - rightOperand,
-  '×': (leftOperand, rightOperand) => leftOperand * rightOperand,
-  '÷': (leftOperand, rightOperand) => leftOperand / rightOperand,
+import binaryOperators from './binaryOperators';
+
+const performBinaryOperation = (formula, widestOperatorIndex) => {
+  // perform binary operation
+  return formula.map((symbol, symbolIndex) => {
+    if (symbolIndex === widestOperatorIndex) {
+      return binaryOperators[formula[widestOperatorIndex]](
+        formula[widestOperatorIndex - 1], formula[widestOperatorIndex + 1]
+      );
+    }
+    return symbol;
+  }).filter((symbol, symbolIndex) => {
+    // if operands were nested in brackets, remove the brackets
+    if ((symbolIndex === widestOperatorIndex + 2 && symbol === ')')
+      || (symbolIndex === widestOperatorIndex - 2 && symbol === '(')) {
+      return false;
+    }
+    // remove operands
+    return symbolIndex !== widestOperatorIndex + 1 && symbolIndex !== widestOperatorIndex - 1;
+  });
 };
 
 export default performBinaryOperation;
