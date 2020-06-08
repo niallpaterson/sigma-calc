ae985c74cbbcc01580358c3912282b9df833d5f5
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-08 09:41:53 +0100

    fix: Unary substractions evaluated for binary operations

--- js/calculateResult.js
+++ js/calculateResult.js
@@ -1,13 +1,15 @@
-import isBinaryOperator from './isBinaryOperator';
-import performBinaryOperation from './performBinaryOperation';
-import applyUnarySubtraction from './applyUnarySubtraction';
-import getIndexOfWidestScopeBinaryOperator from './getWidestScopeBinaryOperator'
+import isBinaryOperator from './isBinaryOperator.js';
+import performBinaryOperation from './performBinaryOperation.js';
+import applyUnarySubtraction from './applyUnarySubtraction.js';
+import getIndexOfWidestScopeBinaryOperator from './getWidestScopeBinaryOperator.js'
 
 const calculateResult = (formula) => {
   if (formula.some((symbol) => isBinaryOperator(symbol))) {
     const removedUnaries = applyUnarySubtraction(formula);
-    const widestOperatorIndex = getIndexOfWidestScopeBinaryOperator(removedUnaries);
-    return calculateResult(performBinaryOperation(formula, widestOperatorIndex));
+    if (removedUnaries.some((symbol) => isBinaryOperator(symbol))) {
+      const widestOperatorIndex = getIndexOfWidestScopeBinaryOperator(removedUnaries);
+      return calculateResult(performBinaryOperation(formula, widestOperatorIndex));
+    } return removedUnaries;
   }
   return formula[0];
 };
