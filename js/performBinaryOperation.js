3997ac63dc58f6de834907bbb2d054c79eb437f3
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-08 20:12:33 +0100

    fix: Described narrowest scope as widest

--- js/performBinaryOperation.js
+++ js/performBinaryOperation.js
@@ -1,22 +1,22 @@
 import binaryOperators from './binaryOperators.js';
 
-const performBinaryOperation = (formula, widestOperatorIndex) => {
+const performBinaryOperation = (formula, narrowestOperatorIndex) => {
   // perform binary operation
   return formula.map((symbol, symbolIndex) => {
-    if (symbolIndex === widestOperatorIndex) {
-      return binaryOperators[formula[widestOperatorIndex]](
-        formula[widestOperatorIndex - 1], formula[widestOperatorIndex + 1]
+    if (symbolIndex === narrowestOperatorIndex) {
+      return binaryOperators[formula[narrowestOperatorIndex]](
+        formula[narrowestOperatorIndex - 1], formula[narrowestOperatorIndex + 1]
       );
     }
     return symbol;
   }).filter((symbol, symbolIndex) => {
     // if operands were nested in brackets, remove the brackets
-    if ((symbolIndex === widestOperatorIndex + 2 && symbol === ')')
-      || (symbolIndex === widestOperatorIndex - 2 && symbol === '(')) {
+    if ((symbolIndex === narrowestOperatorIndex + 2 && symbol === ')')
+      || (symbolIndex === narrowestOperatorIndex - 2 && symbol === '(')) {
       return false;
     }
     // remove operands
-    return symbolIndex !== widestOperatorIndex + 1 && symbolIndex !== widestOperatorIndex - 1;
+    return symbolIndex !== narrowestOperatorIndex + 1 && symbolIndex !== narrowestOperatorIndex - 1;
   });
 };
 
