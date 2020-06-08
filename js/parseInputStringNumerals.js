b511f9e730de6fb7839ef9d8761c623b30adfdc7
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-08 10:09:35 +0100

    feat: Handles multidigit inputs

--- js/parseInputStringNumerals.js
+++ js/parseInputStringNumerals.js
@@ -1,5 +1,16 @@
 const parseInputStringNumerals = (formula) => {
-  return formula.map((symbol) => (Number(symbol) || Number(symbol) === 0 ? Number(symbol) : symbol));
+  console.log(formula);
+  const joinedStrings = [...formula];
+  joinedStrings.forEach((symbol, symbolIndex) => {
+    if (Number(symbol) || symbol === '.') {
+      if (Number(joinedStrings[symbolIndex + 1]) || joinedStrings[symbolIndex + 1] === '.') {
+        joinedStrings.splice(symbolIndex, 1, symbol + joinedStrings[symbolIndex + 1]);
+        joinedStrings.splice(symbolIndex + 1, 1);
+      }
+    }
+  });
+  console.log(joinedStrings);
+  return joinedStrings.map((symbol) => (Number(symbol) || Number(symbol) === 0 ? Number(symbol) : symbol));
 };
 
 export default parseInputStringNumerals;
