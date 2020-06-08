809ae0c87928d904d6af914e44c4649c5e7b2c70
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-08 20:04:20 +0100

    feat: Add elements object

---
+++ js/elements.js
@@ -0,0 +1,13 @@
+const elements = {
+  formulaInput: document.querySelector('.formula-input'),
+  numerals: document.querySelectorAll('.numeral'),
+  operators: document.querySelectorAll('.operator'),
+  bigOperators: document.querySelectorAll('bigOperator'),
+  brackets: document.querySelectorAll('.bracket'),
+  ACdelete: document.querySelector('#AC-btn'),
+  delete: document.querySelector('#del-btn'),
+  period: document.querySelector('#period'),
+  equals: document.querySelector('#equals'),
+};
+
+export default elements;
