817bebb93e7eb5e32989047007d9e40daf66ad81
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-15 22:56:11 +0100

    test: Add tests for negative number inverter

---
+++ tests/unit/invertNegativeNumbers.test.js
@@ -0,0 +1,19 @@
+import invertNegativeNumbers from '../../js/invertNegativeNumbers';
+
+describe('correctly negates unary instances of subtraction', () => {
+  test('no brackets', () => {
+    expect(invertNegativeNumbers(['−', 2])).toEqual([-2]);
+  });
+});
+
+describe('does not negate binary instances of substraction', () => {
+  test('no brackets', () => {
+    expect(invertNegativeNumbers([1, '−', 2])).toEqual([1, '−', 2]);
+  });
+});
+
+describe('leaves intact formulae with no subtraction', () => {
+  test('no brackets', () => {
+    expect(invertNegativeNumbers([2, '+', 2])).toEqual([2, '+', 2]);
+  });
+});
