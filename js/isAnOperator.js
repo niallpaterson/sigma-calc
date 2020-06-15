01894b6e5e6916f343e9bff048de97f0bf6af645
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-15 16:43:29 +0100

    refactor: Operator checker checks operator object

--- js/isAnOperator.js
+++ js/isAnOperator.js
@@ -1,3 +1,5 @@
-const isAnOperator = (symbol) => symbol === '+' || symbol === '−' || symbol === '×' || symbol === '÷' || symbol === '√' || symbol === '∗∗';
+import operators from './operators.js';
+
+const isAnOperator = (symbol) => operators.hasOwnProperty(symbol);
 
 export default isAnOperator;
