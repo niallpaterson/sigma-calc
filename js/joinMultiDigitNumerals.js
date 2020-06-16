ed44d263cf8e1d42251bdb87f9418270f417882d
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-16 19:08:38 +0100

    refactor: Make multidigit numeral join func pure

--- js/joinMultiDigitNumerals.js
+++ js/joinMultiDigitNumerals.js
@@ -1,13 +1,25 @@
-const joinMultiDigitNumerals = (formula, symbol, index) => {
-  if ((Number(symbol) || symbol === '0')
-  && (Number(formula[index + 1]) 
-  || formula[index + 1] === '0' 
-  || formula[index + 1] === '.')) {
-    formula[index] += formula[index + 1];
-    formula.splice(index + 1, 1);
-    return joinMultiDigitNumerals(formula, symbol, index);
-  }
-  return formula;
+import isNumeralString from './isNumeralString.js';
+
+const joinMultiDigitNumerals = (formula) => {
+  const staleNumeralIndexes = [];
+  return formula.map((symbol, index) => {
+    if (isNumeralString(symbol)) {
+      let joinedNumerals = symbol;
+      for (let i = index + 1; i < formula.length; i++) {
+        if (isNumeralString(formula[i])) {
+          joinedNumerals += formula[i];
+          staleNumeralIndexes.push(i);
+        }
+        // only concantenate up to the end of each multidigit numeral
+        if (!isNumeralString(formula[i])) { break; }
+      }
+      return joinedNumerals;
+    }
+    return symbol;
+  }).filter((symbol, index) => {
+    if (staleNumeralIndexes.includes(index)) { return false; }
+    return true;
+  });
 };
 
 export default joinMultiDigitNumerals;
