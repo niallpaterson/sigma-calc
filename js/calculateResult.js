83360672dd748ebe0be09677c686ba861f3c9707
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-16 19:34:26 +0100

    refactor: Now handles bodmas correctly

--- js/calculateResult.js
+++ js/calculateResult.js
@@ -1,19 +1,29 @@
-import isBinaryOperator from './isBinaryOperator.js';
-import performBinaryOperation from './performBinaryOperation.js';
-import applyUnarySubtraction from './applyUnarySubtraction.js';
-import getIndexOfnarrowestScopeBinaryOperator from './getnarrowestScopeBinaryOperator.js'
+import isAnOperator from './isAnOperator.js';
+import outputIsValidNumber from './outputIsValidNumber.js';
+import performOperation from './performOperation.js';
+import invertNegativeNumbers from './invertNegativeNumbers.js';
+import getIndexesOfNarrowestOperators from './getIndexesOfNarrowestOperators.js'
+import bodmasArray from './bodmasArray.js';
 
 const calculateResult = (formula) => {
-  if (formula.some((symbol) => isBinaryOperator(symbol))) {
-    const removedUnaries = applyUnarySubtraction(formula);
-    if (removedUnaries.some((symbol) => isBinaryOperator(symbol))) {
-      const narrowestOperatorIndex = getIndexOfnarrowestScopeBinaryOperator(removedUnaries);
-      return calculateResult(performBinaryOperation(removedUnaries, narrowestOperatorIndex));
-    } return removedUnaries;
+  if (formula.some((symbol) => isAnOperator(symbol))) {
+    formula = invertNegativeNumbers(formula);
   }
-  // passing output through Number() catches various syntax errors
-  // e.g., when output includes 'undefined'
-  return Number(formula[0]);
+
+  for (let i = 0; i < bodmasArray.length; i++) {
+    for (let j = 0; j < formula.length; j++) {
+      if (isAnOperator(formula[j]) &&
+        formula[j] === bodmasArray[i] &&
+        getIndexesOfNarrowestOperators(formula).includes(j)) {
+        formula = performOperation(formula, j);
+        return calculateResult(formula);
+      }
+    }
+  }
+
+  if (outputIsValidNumber((formula))) { return formula[0]; }
+
+  return NaN;
 };
 
 export default calculateResult;
