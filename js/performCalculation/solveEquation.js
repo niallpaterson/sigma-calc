de5dc7961635426f17690f0260fbb06a1cb83fad
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-24 19:30:33 +0100

    fix: Incorrect pi symbol in antecedent

--- js/performCalculation/solveEquation.js
+++ js/performCalculation/solveEquation.js
@@ -5,7 +5,7 @@ const equation = {
   activeBigOperator: 'Σ',
   solve(formula, lowerLimit, upperLimit) {
     if (this.activeBigOperator === 'Σ') { return bigOperators.sigma(formula, lowerLimit, upperLimit); }
-    if (this.activeBigOperator === 'П') { return bigOperators.pi(formula, lowerLimit, upperLimit); }
+    if (this.activeBigOperator === 'Π') { return bigOperators.pi(formula, lowerLimit, upperLimit); }
     return 'Error: No Big Operator Active';
   },
 };
