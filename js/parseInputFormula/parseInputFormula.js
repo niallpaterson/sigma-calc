9d6b1c8935f02f34161039a9ea3e6c53da0ac856
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-03 13:41:05 +0100

    refactor(parseinput): use compose function

--- js/parseInputFormula/parseInputFormula.js
+++ js/parseInputFormula/parseInputFormula.js
@@ -2,13 +2,16 @@ import joinMultiDigitNumerals from './units/joinMultiDigitNumerals.js';
 import convertNumStringsToNumerals from './units/convertNumStringsToNumerals.js';
 import splitInputFormula from './units/splitInputFormula.js';
 import invertNegativeNumbers from './units/invertNegativeNumbers.js';
-import parseExponents from './units/parseExponents.js'
+import parseExponents from './units/parseExponents.js';
 
-const parseInputFormula = (inputFormula) => {
-  return parseExponents(invertNegativeNumbers(
-    convertNumStringsToNumerals(
-      joinMultiDigitNumerals(
-        splitInputFormula(inputFormula)))));
-};
+const compose = (...funcs) => (input) => funcs.reduceRight((acc, func) => func(acc), input);
+
+const parseInputFormula = compose(
+  parseExponents,
+  invertNegativeNumbers,
+  convertNumStringsToNumerals,
+  joinMultiDigitNumerals,
+  splitInputFormula,
+);
 
 export default parseInputFormula;
