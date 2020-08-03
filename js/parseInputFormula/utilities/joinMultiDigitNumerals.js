4f9bbf182aa40220a0677f64c7304d073487e2ae
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-03 19:05:37 +0100

    refactor(parseinput): use reduce + map
    for isNumeralString

--- js/parseInputFormula/utilities/joinMultiDigitNumerals.js
+++ js/parseInputFormula/utilities/joinMultiDigitNumerals.js
@@ -1,25 +1,11 @@
 import isNumeralString from '../isNumeralString.js';
 
-const joinMultiDigitNumerals = (formula) => {
-  const staleNumeralIndexes = [];
-  return formula.map((symbol, index) => {
-    if (isNumeralString(symbol)) {
-      let joinedNumerals = symbol;
-      for (let i = index + 1; i < formula.length; i++) {
-        if (isNumeralString(formula[i])) {
-          joinedNumerals += formula[i];
-          staleNumeralIndexes.push(i);
-        }
-        // only concantenate up to the end of each multidigit numeral
-        if (!isNumeralString(formula[i])) { break; }
-      }
-      return joinedNumerals;
-    }
-    return symbol;
-  }).filter((symbol, index) => {
-    if (staleNumeralIndexes.includes(index)) { return false; }
-    return true;
-  });
-};
+const joinMultiDigitNumerals = (formula) => formula
+.reduce((acc, symbol, index, formula) => {
+    if (index === 0) { return [symbol] }
+    if (!isNumeralString(symbol)) { return [...acc, symbol] }
+    if (!isNumeralString(formula[index - 1])) { return [...acc, symbol] }
+    return acc.map((item, j) => (j === acc.length - 1) ? (item + symbol) : item);
+  }, []);
 
 export default joinMultiDigitNumerals;
