0fd86afdcb2ed1c005acccaf4ba76564bccc41ce
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-14 11:34:59 +0100

    chore: add directory barrells
    issue #55

--- js/parser/parsers/index.js
+++ js/parser/parsers/index.js
@@ -3,11 +3,17 @@ import invertNegativeNumbers from './invertNegativeNumbers.js';
 import joinMultiDigitNumerals from './joinMultiDigitNumerals.js';
 import parseExponents from './parseExponents.js';
 import replaceFormulaVariables from './replaceFormulaVariables.js';
+import validateOutput from './validateOutput.js';
+import trimBrackets from './trimBrackets.js';
+import normaliseZeros from './normaliseZeros.js';
 
 export default {
   convertNumStringsToNumerals,
+  trimBrackets,
   invertNegativeNumbers,
   joinMultiDigitNumerals,
   parseExponents,
   replaceFormulaVariables,
+  validateOutput,
+  normaliseZeros,
 };
